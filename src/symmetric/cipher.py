ff61945204a7a087fb6c60abc87892ce66344e80
Pedenite <pedenite@gmail.com>
2021-09-05 16:52:20 -0300

    funçoes do AES

--- src/symmetric/cipher.py
+++ src/symmetric/cipher.py
@@ -1,2 +1,16 @@
-def cifra(msg, key):
+def cifra(msg, key, rounds):
+
+    for i in rounds:
+        pass
+
+def subBytes():
+    pass
+
+def shiftRows():
+    pass
+
+def mixColumns():
+    pass
+
+def addRoundKey(msg, key):
     pass
